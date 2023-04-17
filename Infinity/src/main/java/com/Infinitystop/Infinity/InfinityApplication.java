f99259e81546fbf4e86ba819546f850c090d2d85
Ashish <anshm9448@gmail.com>
2023-04-17 19:58:43 +0530

    database

--- Infinity/src/main/java/com/Infinitystop/Infinity/InfinityApplication.java
+++ Infinity/src/main/java/com/Infinitystop/Infinity/InfinityApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InfinityApplication {
+public class
+InfinityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InfinityApplication.class, args);
